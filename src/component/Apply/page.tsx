e5e5d3b4dfd2958135c12eb400199182a0d87cdd
Ashley y <ashleynda>
2025-03-04 12:23:01 +0100

    updating

--- src/component/Apply/page.tsx
+++ src/component/Apply/page.tsx
@@ -5,8 +5,8 @@ import { IoPersonOutline } from "react-icons/io5";
 
 export default function Apply () {
     return (
-        <div className="flex flex-wrap justify-center items-center gap-4 p-6 bg-[#EBF3FF]">
-            <div className="bg-white p-4 rounded-xl shadow-lg flex flex-col justify-between items-center text-center w-full sm:w-64 md:w-72 h-56">
+        <div className="flex flex-wrap justify-center items-center bg-[#EBF3FF]">
+            <div className="bg-white p-4 rounded-xl shadow-lg flex flex-col justify-center items-center text-center w-full sm:w-64 md:w-72 h-56">
                 <div className="text-sky-500 text-3xl">
                     <IoPersonOutline />
                 </div>
@@ -18,7 +18,7 @@ export default function Apply () {
                 </button>
             </div>
 
-            <div className="bg-white p-4 rounded-xl shadow-lg flex flex-col justify-between items-center text-center w-full sm:w-64 md:w-72 h-56">
+            <div className="bg-white p-4 rounded-xl shadow-lg flex flex-col items-center text-center w-full sm:w-64 md:w-72 h-56">
                 <div className="text-sky-500 text-3xl">
                     <FaPeopleGroup />
                 </div>
--- src/component/Footer/page.tsx
+++ src/component/Footer/page.tsx
@@ -4,7 +4,6 @@ export default function Footer() {
     return (
         <footer className="mt-12 bg-[#051027] py-12 text-white">
         <div className="mx-auto grid max-w-6xl gap-8 px-4 grid-cols-1 md:grid-cols-2">
-            {/* Logistics Section */}
             <div>
                 <h4 className="mb-4 text-lg font-bold text-cyan-500">Logistics</h4>
                 <ul className="space-y-2 list-disc list-inside text-white text-sm sm:text-base">
@@ -16,24 +15,7 @@ export default function Footer() {
                     </li>
                 </ul>
             </div>
-
-            {/* Resources and Support Section */}
-            <div>
-                <h4 className="mb-4 text-lg font-bold text-cyan-500">Resources and Support</h4>
-                <p className="mb-4 text-sm sm:text-base">
-                    Semicolon is hosting the training on Sui Move, sponsored by the Sui Foundation. If you have any questions or need any kind of support, click on the link to register:  
-                    <a className="underline text-cyan-400" href="https://bit.ly/SC-SUI-Training" target="_blank" rel="noopener noreferrer">
-                        https://bit.ly/SC-SUI-Training
-                    </a>
-                </p>
-                <p className="text-sm sm:text-base">
-                    Semicolon is a workforce development company that is solving the problem of youth unemployment by training software engineers and hardware developers through an intensive 12-month program. The company has a proven track record with over 300 software engineers in the global market.
-                </p>
-            </div>
-        </div>
-
-        {/* Logo Section */}
-        <div className="max-w-6xl mx-auto px-4 flex flex-col sm:flex-row items-center justify-center gap-4 p-6 sm:p-12">        
+            <div className="max-w-6xl mx-auto px-4 flex flex-col sm:flex-row items-center justify-center gap-4 p-6 sm:p-12">        
             <Image
                 src="/assets/semicolon-logo.svg"
                 alt="Semicolon Logo"
@@ -48,6 +30,22 @@ export default function Footer() {
                 height={30}
             />
         </div>
+
+            <div>
+                <h4 className="mb-4 text-lg font-bold text-cyan-500">Resources and Support</h4>
+                <p className="mb-4 text-sm sm:text-base">
+                    Semicolon is hosting the training on Sui Move, sponsored by the Sui Foundation. If you have any questions or need any kind of support, click on the link to register:  
+                    <a className="underline text-cyan-400" href="https://bit.ly/SC-SUI-Training" target="_blank" rel="noopener noreferrer">
+                        https://bit.ly/SC-SUI-Training
+                    </a>
+                </p>
+                <p className="text-sm sm:text-base">
+                    Semicolon is a workforce development company that is solving the problem of youth unemployment by training software engineers and hardware developers through an intensive 12-month program. The company has a proven track record with over 300 software engineers in the global market.
+                </p>
+            </div>
+        </div>
+
+       
     </footer>
 
   );
